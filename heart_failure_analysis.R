9b8f7ddb1b32e2beecacd8fb073c1194bd700b67
Joe LaVigne <jmlavigne510@gmail.com>
2024-09-09 15:58:47 -0400

    EDA pt 2 - exploratory graphs added

--- heart_failure_analysis.R
+++ heart_failure_analysis.R
@@ -12,3 +12,33 @@ View(summary(heart))
 print(mean(heart$age))
 print(IQR(heart$age))
 print(min(heart$age))
+
+
+#EDA pt. 2 for heart dataset: dataset visualizations 
+
+plate_vs_cp <- ggplot(heart, aes(heart$platelets, heart$creatinine_phosphokinase)) + geom_point() + ggtitle("Platelets vs. Creatinine phosphokinase Scatterplot") + labs( x="Platelets", y="Creatinine Phosphokinase")
+plate_vs_cp
+
+
+age_hist <- ggplot(heart, aes(heart$age)) + geom_histogram() + ggplot("Age Histogram")
+age_hist
+
+ef_hist <- ggplot(heart, aes(heart$ejection_fraction)) + geom_histogram() + ggtitle("Ejection Fraction Histogram")
+ef_hist
+
+dev.off()
+plot(rnorm(50), rnorm(50))
+
+smoking_SC_box <- ggplot(heart, aes(y=heart$serum_creatinine, x=heart$smoking == 1)) + geom_boxplot() + ggtitle("Smoking vs. Serum Creatinine Boxplot") + labs( x="smoking status", y="Serum Creatinine")
+smoking_SC_box
+
+
+smoking_plates_box <- ggplot(heart, aes(y=heart$platelets, x=heart$smoking == 1)) + geom_boxplot() + ggtitle("Smoking vs. Platelets Boxplot") + labs( x="smoking status", y="Platelets")
+smoking_plates_box
+
+sc_hist <- ggplot(heart, aes(heart$serum_creatinine)) + geom_histogram() +ggtitle("Serum Creatinine Histogram")
+sc_hist
+
+
+
+
