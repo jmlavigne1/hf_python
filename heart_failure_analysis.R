6c4e3f182fa7a6915889323c245cb01c72e08eb8
Joe LaVigne <jmlavigne510@gmail.com>
2024-09-09 13:45:19 -0400

    EDA pt 1 added for descriptive stats

--- heart_failure_analysis.R
+++ heart_failure_analysis.R
@@ -5,3 +5,10 @@ library(ggplot2)
 heart <- read_csv('heart_failure_clinical_records_dataset.csv')
 
 print(head(heart))
+
+
+#EDA pt. 1 for heart dataset: descriptive statistics
+View(summary(heart))
+
+
+
