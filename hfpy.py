a2d8493b7431fec7b5f3ad612c752f01c2e52757
Joe LaVigne <jmlavigne510@gmail.com>
2024-09-16 06:51:55 -0400

    hfpy code bloc prepped for statistical testing

---
+++ hfpy.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 06:45:47 2024
+
+@author: lavig
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+hfpy = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+
+print(hfpy)
+
+
+# EDA 
+
+
+
+
+# chi-squared test to test for association 
