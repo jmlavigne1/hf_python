bfee851967f41c91be6b78e01c1218eedaee426b
Joe LaVigne <jmlavigne510@gmail.com>
2024-09-16 17:45:31 -0400

    README and project testing parameters added to code block

---
+++ README_py.md
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 17:41:38 2024
+
+@author: lavig
+"""
+
+##README
+
+#the purpose of this project is to demonstrate hypothesis testing for:
+    #1.) two sample t-test
+    #2.) ANOVA and Tukey
+    #3.) Chi-squared test
--- hfpy.py
+++ hfpy.py
@@ -7,7 +7,7 @@ Created on Mon Sep 16 06:45:47 2024
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 hfpy = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
@@ -17,6 +17,20 @@ print(hfpy)
 # EDA 
 
 
+print(hfpy.head())
 
+print(hfpy.dtypes)
+
+# scatterplots of various features
+
+plt.plot(hfpy.creatinine_phosphokinase)
+plt.clf()
+plt.scatter(hfpy.platelets, hfpy.creatinine_phosphokinase)
+plt.show()
+
+plt.scatter(hfpy.serum_sodium, hfpy.creatinine_phosphokinase)
+plt.show()
+
+plt.scatter(hfpy.serum_sodium, hfpy.serum_creatinine)
 
 # chi-squared test to test for association 
