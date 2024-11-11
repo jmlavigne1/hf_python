0cd4b6d988e53032794081560d57afbc9d498fbb
Joe LaVigne <jmlavigne510@gmail.com>
2024-11-11 07:53:59 -0500

    initial plotting

--- HF_kmeans.py
+++ HF_kmeans.py
@@ -5,9 +5,22 @@ Created on Sun Nov 10 15:15:52 2024
 @author: lavig
 """
 
-from sklearn import KMeans
+from sklearn.cluster import KMeans
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 hf = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 print(hf.head())
+
+print(hf.columns)
+
+x = hf.iloc[:, 4]
+y = hf.iloc[:,-1]
+
+plt.scatter(x, y, alpha=0.5)
+plt.show()
+
+
+model = KMeans(n_clusters=2)
