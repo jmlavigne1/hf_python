cb3d1a27903fcd68b13befcbe293cac49e7fc007
Joe LaVigne <jmlavigne510@gmail.com>
2024-11-12 20:57:05 -0500

    model made

--- HF_kmeans.py
+++ HF_kmeans.py
@@ -10,6 +10,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import os
+os.environ["OMP_NUM_THREADS"] = '4'
 
 hf = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 print(hf.head())
@@ -24,11 +26,26 @@ cv = lambda x: np.std(x, ddof=1)/np.mean(x)*100
 print(hf.apply(cv))
 
 
-x = hf.iloc[:, 4]
-y = hf.iloc[:,-6]
+x = hf.iloc[:,2] #creatinine_phosphokinase
+y = hf.iloc[:,8] #serum_sodium
 
-plt.scatter(x, y, alpha=0.75)
+plt.scatter(x, y, alpha=0.50)
+plt.title("Creatinine Phosphokinase vs. Serum Sodium")
+plt.xlabel("Serum Creatinine")
+plt.ylabel("Serum Sodium")
 plt.show()
 
 
-model = KMeans(n_clusters=2)
+
+model = KMeans(n_clusters=4)
+model.fit(hf)
+labels = model.predict(hf)
+print(labels)
+
+
+x = hf.iloc[:,2]
+y = hf.iloc[:,8]
+
+
+plt.scatter(x, y, c=labels, alpha=0.5)
+plt.show()
